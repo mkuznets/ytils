8c4d8870b026e3cd8d38ef43e1ab4676349ba384
Max Kuznetsov <maks.kuznetsov@gmail.com>
2023-06-03 02:00:11 +0100

    ycli: print internal errors; do not use slog; make Init() and Validate() optional

--- ycli/command.go
+++ ycli/command.go
@@ -1,55 +1,74 @@
 package ycli
 
 import (
+	"fmt"
 	"github.com/jessevdk/go-flags"
-	"golang.org/x/exp/slog"
 	"os"
 )
 
-type Command[T any] interface {
+// PrintErrors is intentionally excluded because it is not applied to internal errors
+// (such as default value on a boolean flag).
+// We want to print these internal errors, but don't want to print user errors twice.
+var flagsOptions flags.Options = flags.HelpFlag | flags.PassDoubleDash
+
+// Initer is an optional interface for commands that require global flags.
+// The command can read the global values and store them to be later used in Execute.
+type Initer[T any] interface {
 	Init(app *T) error
+}
+
+// Validator is an optional interface for commands that require additional validation of their flags.
+type Validator interface {
 	Validate() error
-	Execute(args []string) error
 }
 
 func Main[T any]() *T {
 	var app T
-	var parser = flags.NewParser(&app, flags.Default)
+	var parser = flags.NewParser(&app, flagsOptions)
 	parser.CommandHandler = func(command flags.Commander, args []string) error {
 		if command == nil {
 			return nil
 		}
 
-		c := command.(Command[T])
-		if err := c.Validate(); err != nil {
-			slog.Error("invalid arguments", err)
-			os.Exit(1)
+		if validator, ok := command.(Validator); ok {
+			if err := validator.Validate(); err != nil {
+				errorExit(err)
+			}
 		}
 
-		if err := c.Init(&app); err != nil {
-			slog.Error("init", err)
-			os.Exit(1)
+		if initer, ok := command.(Initer[T]); ok {
+			if err := initer.Init(&app); err != nil {
+				errorExit(err)
+			}
 		}
 
-		if err := c.Execute(args); err != nil {
-			slog.Error("command error", err)
-			os.Exit(1)
+		if err := command.Execute(args); err != nil {
+			errorExit(err)
 		}
 
 		return nil
 	}
 
 	if _, err := parser.Parse(); err != nil {
-		if e, ok := err.(*flags.Error); ok {
-			if e.Type == flags.ErrHelp {
-				os.Exit(0)
-			} else {
-				os.Exit(1)
-			}
-		}
-		slog.Error("invalid arguments", err)
-		os.Exit(1)
+		errorExit(err)
 	}
 
 	return &app
+}
+
+func errorExit(err error) {
+	if err == nil {
+		return
+	}
+
+	flagsErr, ok := err.(*flags.Error)
+	if ok && flagsErr.Type == flags.ErrHelp {
+		_, _ = fmt.Fprintln(os.Stdout, err)
+		os.Exit(0)
+	} else {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	return
 }
